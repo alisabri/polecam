4b9abaee82a5f05f5e264d26218983ce699bd09b
alisabri <alisabrinal@gmail.com>
2017-10-19 23:35:00 +0300

    cont.

--- js/controller/scheduleController.js
+++ js/controller/scheduleController.js
@@ -1,10 +1,36 @@
 define(["app"], function(app) {
 
     function init(query) {
-
+        console.log(app);
+        var pickerDescribe = app.f7.picker({
+            input: '#picker-describe',
+            rotateEffect: true,
+            cols: [{
+                    textAlign: 'left',
+                    values: (function() {
+                        var retval = [];
+                        for (i = 0; i < 24; i++)
+                            retval.push(i);
+                        return retval;
+                    })(),
+                },
+                {
+                    divider: true,
+                    content: ':'
+                },
+                {
+                    values: (function() {
+                        var retval = [];
+                        for (i = 0; i < 60; i++)
+                            retval.push(i);
+                        return retval;
+                    })(),
+                },
+            ]
+        });
     }
     return {
-      init: init
+        init: init
     };
 
 });
